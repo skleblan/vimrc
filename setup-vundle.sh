f9ddab2a771b225a689d17f0cace306e996f1a0d
Steven LeBlanc <skleblan@gmail.com>
2019-04-10 11:26:04 -0500

    Add untested script to initialize Vundle directories

---
+++ setup-vundle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#TODO: reminders about extra install steps for plugins
+#TODO: prompt warning at beginning?
