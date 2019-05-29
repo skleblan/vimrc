519d218475957f6d49db39a4309d8db087e4307a
Steven LeBlanc <skleblan@gmail.com>
2019-05-29 18:52:28 +0000

    Create Links and install YouCompleteMe
    create symbolic links in current directory for
    convenience. Starts installation of an advanced
    plugin (YouCompleteMe) that requires additional steps.
    Will fail if build dependencies aren't met. On Ubuntu
    those are the deb packages for build-essential, cmake,
    and python3-dev.
    
    Also added a gitignore to exclude the symbolic links

--- setup-vundle.sh
+++ setup-vundle.sh
@@ -6,5 +6,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
 vim +PluginInstall +qall
 
+ln -s ~/.vim/bundle bundle.lnk
+ln -s ~/.vimrc vimrc.lnk
+
+~/.vim/bundle/YouCompleteMe/install.py
+
 #TODO: reminders about extra install steps for plugins
 #TODO: prompt warning at beginning?
