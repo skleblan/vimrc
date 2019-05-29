a6ffb7e23a7c120286a92900931d38f0ea0172cc
Steven LeBlanc <skleblan@gmail.com>
2019-05-29 18:49:40 +0000

    Copy over vimrc and run Vundle's Install
    Copies vimrc to home directory and renames it
    accordingly. Uses vim's ability to pass commands on
    the command line to start the Installation for Vundle
    and any other basic plugins. Returns back to command
    line when installation finishes.

--- setup-vundle.sh
+++ setup-vundle.sh
@@ -3,5 +3,8 @@
 mkdir -p ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+cp vimrc ~/.vimrc
+vim +PluginInstall +qall
+
 #TODO: reminders about extra install steps for plugins
 #TODO: prompt warning at beginning?
